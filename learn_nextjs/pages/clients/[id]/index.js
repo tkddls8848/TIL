3d447f11916ebadb630825811d6c17a630c39998
PSI <tkddls8848@gmail.com>
2022-07-30 18:17:45 +0900

    make button by router.push function

--- learn_nextjs/pages/clients/[id]/index.js
+++ learn_nextjs/pages/clients/[id]/index.js
@@ -2,12 +2,21 @@ import { useRouter } from "next/router";
 
 function ClientPage() {
   const router = useRouter();
+  const buttonHandler = () => {
+    router.push({
+      pathname: '/clients/[id]/[clientname]',
+      query: {id: router.query.id, clientname: "test"}
+    });
+  };
 
   return (
       <div>
         <h1>This is Simple ClientPage</h1>
         <div>
           your user id is <b>{router.query.id}</b>
+          <button onClick={buttonHandler}>
+            Jump to info
+          </button>
         </div>
       </div>
     );
