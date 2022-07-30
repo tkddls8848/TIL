e37c8bdc3620711e4fbd4d290b55d4db15a4b4e2
PSI <tkddls8848@gmail.com>
2022-07-30 16:47:02 +0900

    navigate by Link

--- learn_nextjs/pages/index.js
+++ learn_nextjs/pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>This is HomePage</h1>
+      <ul>
+        <li>
+          <a href="/portfolio">link through a tag</a>
+        </li>
+        <li>
+          <Link href="/portfolio">link through Link tag</Link>
+        </li>
+      </ul>      
     </div>
   );
 };
--- learn_nextjs/pages/portfolio/index.js
+++ learn_nextjs/pages/portfolio/index.js
@@ -2,6 +2,7 @@ function PortfolioPage() {
     return (
       <div>
         <h1>This is PortfolioPage</h1>
+        <a href="/">To home</a>
       </div>
     );
   };
