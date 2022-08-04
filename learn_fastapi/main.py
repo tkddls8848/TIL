449e7edb0040621a1a7a557b0f170d46cad611b4
PSI <tkddls8848@gmail.com>
2022-08-04 22:51:52 +0900

    Merge branch 'fastapi'

--- learn_fastapi/main.py
+++ learn_fastapi/main.py
@@ -3,11 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
+##URL경로명을 이용한 호출
 @app.get("/")
 async def home():
     return {"message": "Hello World"}
@@ -20,6 +16,11 @@ async def read_user():
 async def read_user_id(user_id: str):
     return {"user_id" : user_id}
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     print(model_name)
@@ -31,3 +32,11 @@ async def get_model(model_name: ModelName):
         return {"model_name" : model_name, "message": "This model is lenet"}  
     else:
         return {"model_name" : model_name, "message": "ETC model"}  
+    
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+##쿼리스트링을 이용한 호출
+@app.get("/items/")
+async def read_item(key: int = 0, value: int =10):
+    print(key, value)
+    return {"get_item_by_key" : fake_items_db[key], "get_query_by value": value}
