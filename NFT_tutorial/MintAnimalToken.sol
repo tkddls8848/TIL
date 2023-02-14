31025b0297a2ebed0b60ebef87b99ff4a5e2c972
PSI <tkddls8848@naver.com>
2023-02-14 22:51:33 +0900

    make NFT tutorial

---
+++ NFT_tutorial/MintAnimalToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract MintAnimalToken is ERC721Enumerable {
+    constructor() ERC721("PSIAnimalToken", "PAT") {}
+
+    mapping(uint256 => uint256) public animalTypes;
+
+        function mintAnimalToken() public {
+            uint256 animalTokenId = totalSupply() + 1;
+            uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 10 + 1;
+            animalTypes[animalTokenId] = animalType;
+            _mint(msg.sender, animalTokenId);
+        }
+}
